56bf7655435a1f0ecdec32c82c847c7236ed7751
= <markdw88@gmail.com>
2024-05-06 00:52:49 -0400

    Fixed

--- raylib-bazel/raylib-bazel.cpp
+++ raylib-bazel/raylib-bazel.cpp
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[]) {
   const int screenHeight = 450;
 
   InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-  SetTargetFPS(60);  // Set our game to run at 60 frames-per-second
+  SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
